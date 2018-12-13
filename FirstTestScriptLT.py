48940f1bc284587902846e8962a5b7e0eaaae78b
polarbear71 <45788246+polarbear71@users.noreply.github.com>
2018-12-13 09:30:45 +0100

    Upload initial script

---
+++ FirstTestScriptLT.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("Hello world")
+wortliste = ["eins", "zwei", "drei"]
+print(wortliste)
+for wort in wortliste:
+    print(wort)
+  
