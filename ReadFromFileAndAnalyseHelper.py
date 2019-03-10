e37c92daddcf18c3563b42de80084667e10dc6aa
polarbear71 <45788246+polarbear71@users.noreply.github.com>
2019-03-10 20:47:42 +0100

    Add files via upload

---
+++ ReadFromFileAndAnalyseHelper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class NameAmount:
+    def __init__(self, IdNumber, name, amount):
+        self.Id = IdNumber
+        self.Name = name
+        self.Amount = amount 
+        
+    def setId(self, IdNumber):
+        self.Id = IdNumber
+        
+    def setName(self, name):
+        self.Name = name
+        
+    def setAmount(self, amount):
+        self.Amount = amount
+        
+
+
+# In[ ]:
+
+
+
+
